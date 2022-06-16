816a03189b4be62fd2f03b1c18f6b76898a61cbf
Sébastien Giordano <sebastiengiordano@yahoo.fr>
2022-06-16 12:09:35 +0200

    Assertion test update.

--- src/libs/tests/assertion/assertion.cpp
+++ src/libs/tests/assertion/assertion.cpp
@@ -6,11 +6,14 @@
 
 TEST_GROUP(Assertion) {
 
+    AssertionMock* assertion;
+
     void setup() {
-        
+        assertion = new AssertionMock();
     }
 
     void teardown() {
+        delete assertion;
         mock().checkExpectations();
         mock().clear();
     }
@@ -19,27 +22,23 @@ TEST_GROUP(Assertion) {
 TEST(Assertion, assertion_failed_during_test) {
     
     // // Init test.
-    AssertionMock assertion = AssertionMock();
-    assertion.set_target_type(Assertion::TargetType::Host);
-    assertion.set_test_behavior();
-    assertion.check(false, ASSERT_INFO("Assertion failed during test."));
+    assertion->set_target_type(Assertion::TargetType::Host);
+    assertion->set_test_behavior();
+    assertion->check(false, ASSERT_INFO("Assertion failed during test."));
 }
 
 TEST(Assertion, assertion_failed_on_host) {
     
     // // Init test.
-    const char* message = "Assertion failed on host.";
-    AssertionMock assertion = AssertionMock();
-    assertion.set_target_type(Assertion::TargetType::Host);
-    mock().expectOneCall("_host_abord").onObject(&assertion);
-    assertion.check(false, ASSERT_INFO(message));
-}
+    assertion->set_target_type(Assertion::TargetType::Host);
+    mock().expectOneCall("_host_abord").onObject(assertion);
+    assertion->check(false, ASSERT_INFO("Assertion failed on host."));
+}   
 
 TEST(Assertion, assertion_failed_on_device) {
     
     // // Init test.
-    AssertionMock assertion = AssertionMock();
-    assertion.set_target_type(Assertion::TargetType::Device);
-    mock().expectOneCall("_host_abord").onObject(&assertion);
-    assertion.check(false, ASSERT_INFO("Assertion failed on device."));
+    assertion->set_target_type(Assertion::TargetType::Device);
+    mock().expectOneCall("_device_abord").onObject(assertion);
+    assertion->check(false, ASSERT_INFO("Assertion failed on device."));
 }
